96a55eeec7794678300fc416d62b357a2c994150
Jonas Kerwyn <jonas.kerwyn@student.hogent.be>
2022-08-18 01:39:26 +0200

    ui updates

--- weatherapp/src/contexts/WeatherProvider.tsx
+++ weatherapp/src/contexts/WeatherProvider.tsx
@@ -15,29 +15,17 @@ export default function WeatherProvider(props: { children: any }) {
   
   // Options
   const [system, setSystem] = useState(localStorage.getItem('system') ? localStorage.getItem('system') : 'metric');
-  const [language, setLanguage] = useState(localStorage.getItem('lang') ? localStorage.getItem('lang') : 'nl');
 
   const getWeatherInfo = useCallback(async () => {
     try {
       setLoading(true);
       setError('');
-      if (language === '') {
-        setData((await weatherAPI.getCurrentWeatherViaIp()).data);
-      } else {
-        setData((await weatherAPI.getCurrentWeatherViaIpDutch()).data);
-      }
       setData((await weatherAPI.getCurrentWeatherViaIp()).data);
     } catch (error: any) {
       setError(error);
     } finally {
       setLoading(false);
     }
-  }, [language]);
-
-  // Changes the language of the app
-  const changeLang = useCallback((value: string) => {
-    setLanguage(value);
-    localStorage.setItem('lang', value);
   }, []);
   // Changes the usage of the metric or imperial system
   const changeSystem = useCallback((value: string) => {
@@ -47,8 +35,8 @@ export default function WeatherProvider(props: { children: any }) {
 
   // Provider values
   const values = useMemo(() => ({
-    data, loading, error, getWeatherInfo, language, changeLang, system, changeSystem
-  }), [changeLang, changeSystem, data, error, getWeatherInfo, language, loading, system]);
+    data, loading, error, getWeatherInfo, system, changeSystem
+  }), [changeSystem, data, error, getWeatherInfo, loading, system]);
 
   return (
     <WeatherContext.Provider value={values}>
